8b147e05fcef0d15bd2c68da332f7ab183fceab3
Ayush Gupta <121301901+ayush111195@users.noreply.github.com>
2023-01-15 01:43:53 +0530

    Add files via upload

---
+++ springmvcform/src/main/java/springmvcform/controller/Controller.java
@@ -0,0 +1,36 @@
+package springmvcform.controller;
+
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import springmvcform.controller.model.Student;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+@RequestMapping("/home")
+	public String form( ) {
+		System.out.println("this is student page");
+		return "index";
+		
+	}
+@RequestMapping(path="/handleform",method=RequestMethod.POST)
+public String formHandler(@ModelAttribute("student") Student student , BindingResult result) {
+	if(result.hasErrors())
+	{
+		return"index";
+	}
+	System.out.println(student);
+	//m.addAttribute("student1",student);
+	System.out.println(student.getAddress());
+	return "success";
+	
+}
+@RequestMapping("/file")
+public String staticFile() {
+	return "file";
+	
+}
+}
